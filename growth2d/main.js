96b06e1b843bcc2cc89d038b3c81f3fe5841caf9
jakubfiala <j4kub.fiala@gmail.com>
2020-12-15 12:24:07 +0100

    dunno

--- growth2d/main.js
+++ growth2d/main.js
@@ -13,53 +13,72 @@ const lerpv = (a, b, t) => {
 };
 
 const randomGreen = () => {
-    const r = Math.floor(Math.random() * 100);
-    const g = Math.floor(Math.random() * 50 + 165);
-    const b = Math.floor(Math.random() * 100);
+    const r = Math.floor(Math.random() * 50 + 10);
+    const g = Math.floor(Math.random() * 50 + 105);
+    const b = Math.floor(Math.random() * 50 + 10);
     return `rgb(${r}, ${g}, ${b})`;
 };
 
 const randomGrowthNormal = () => {
     return {
-        x: Math.random() * 1.3 - 0.65,
+        x: Math.random() * 1.5 - 0.75,
         y: -1,
     };
 };
 
-const BENDINESS = 6;
+const randomGrowthSize = () => Math.random() * 30 + 7;
+
+const BENDINESS = 10;
+
+const smoothQuadCurveTo = (c, x0, y0, cx, cy, x, y, lw1, lw2) => {
+    const lwmid = lerp1d(lw1, lw2, 0.5);
+    c.moveTo(x0 + lw1, y0 + lw1);
+    c.quadraticCurveTo(
+        cx + lwmid,
+        cy + lwmid,
+        x + lw2, y + lw2);
+    c.lineTo(x - lw2 * 2, y - lw2 * 2);
+    c.quadraticCurveTo(
+        cx - lwmid * 2,
+        cy - lwmid * 2,
+        x0 - lw1, y0 - lw1);
+    c.closePath();
+    c.fill();
+};
 
 const randomGrowth = (c, x, y) => {
+    c.fillStyle = randomGreen();
+    c.beginPath();
+
     const normal = randomGrowthNormal();
     const bendNormal = randomGrowthNormal();
     const growthSize = randomGrowthSize();
     const x1 = x + normal.x * growthSize;
     const y1 = y + normal.y * growthSize;
 
-    c.beginPath();
-    c.moveTo(x, y);
-    c.lineWidth = 1;
-    c.quadraticCurveTo(
+    smoothQuadCurveTo(c, x, y,
         lerp1d(x, x1, 0.5) + bendNormal.x * BENDINESS,
         lerp1d(y, y1, 0.5) + bendNormal.y * BENDINESS,
-        x1, y1);
-    c.stroke();
+        x1, y1, 0.5, 1);
 
     const x2 = x1 + normal.x * growthSize;
     const y2 = y1 + normal.y * growthSize;
 
-    c.beginPath();
-    c.moveTo(x1, y1);
-    c.lineWidth = 2;
-    c.quadraticCurveTo(
+    smoothQuadCurveTo(c, x1, y1,
         lerp1d(x1, x2, 0.5) - bendNormal.x * BENDINESS,
         lerp1d(y1, y2, 0.5) - bendNormal.y * BENDINESS,
-        x2, y2);
-    c.stroke();
+        x2, y2, 1, 2);
+
+    const x3 = x2 + normal.x * growthSize / 2;
+    const y3 = y2 + normal.y * growthSize / 2;
+
+    smoothQuadCurveTo(c, x2, y2,
+        lerp1d(x2, x3, 0.5) + bendNormal.x * BENDINESS / 2,
+        lerp1d(y2, y3, 0.5) + bendNormal.y * BENDINESS / 2,
+        x3, y3, 2, 0.5);
 
     return [ x2, y2 ];
 }
-
-const randomGrowthSize = () => Math.random() * 20 + 20;
 
 const symbolImage = new Image();
 
@@ -82,11 +101,9 @@ symbolImage.addEventListener('load', () => {
     const symbolPixelCoords = [];
     for (let p = 0; p < pixels.length; p += 4) {
         const value = pixels[p + 3];
-        if (value > 0 && Math.random() > 0.995) {
+        if (value > 0 && Math.random() > 0.99) {
             const x = p / 4 % canvas.width;
             const y = Math.floor(p / 4 / canvas.width);
-
-            c.strokeStyle = randomGreen();
             randomGrowth(c, x, y);
 
             symbolPixelCoords.push({ x, y });
